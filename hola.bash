f6e97b8cd8058a006d47e0f2a5f1529a5875496d
manelNunez <147039916+manelNunez@users.noreply.github.com>
2023-10-31 12:34:24 +0100

    Add files via upload

---
+++ hola.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+# include this boilerplate
+function goto
+{
+    label=$1
+    cmd=$(sed -n "/$label:/{:a;n;p;ba};" $0 | grep -v ':$')
+    eval "$cmd"
+    exit
+}
+start=${1:-"start"}
+
+goto $start
+
+start:
+# your script goes here...
+foo:
+firefox
+goto foo
+
+
+
+
